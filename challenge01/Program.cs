9dc57e2374249d872ae69d1ba2c2f90a79e1fdec
Jenri Atalaya <jenri.atalaya@outlook.com>
2023-11-03 11:11:50 -0500

    first challenge completed

---
+++ challenge01/Program.cs
@@ -0,0 +1,17 @@
+﻿string filepath = "message_01.txt";
+
+if (File.Exists(filepath))
+{
+    string fileContent = File.ReadAllText(filepath);
+
+    var output = fileContent.Trim().ToLower().Split(" ")
+    .GroupBy(word => word)
+    .Select(group => new {
+        Word = group.Key,
+        Count = group.Count()
+    })
+    .ToDictionary(item => item.Word, item => item.Count)
+    .Aggregate("",(accum, current) => $"{accum}{current.Key}{current.Value}");
+
+    Console.WriteLine(output);
+}
